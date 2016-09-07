7537b17e06182b149dfcd96089484a03a04bfbc4
virakkong <virakkong@virakkongs-iMac.local>
2016-09-06 22:15:56 -0700

    custom-filter

---
+++ filter/reverse-filter/reverse-filter.js
@@ -0,0 +1,8 @@
+angular.module('myApp').filter('reverse',reverse);
+function reverse () {
+    return function(string) {
+        if (string) {
+            return string.split('').reverse().join('');
+        }
+    }
+}
--- main/main-controller.js
+++ main/main-controller.js
@@ -6,5 +6,10 @@ function MainController(FilmFactory) {
         //we no longer use response.data because
         //in the dataFactory.js, we has function complete(response) 
         //that return response.data; already
-    })
+    });
+    vm.date1 ='12 February 2016';
+    vm.date2='11 March 2016';
+    vm.date3="3 January 20015";
+    vm.date4="25 April 2014";
+    
 }
